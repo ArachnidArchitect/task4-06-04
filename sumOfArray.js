11b850d289665297f37f7195c7e31dc523a3ae49
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-06-05 09:57:32 +0200

    task 4 with html

--- sumOfArray.js
+++ sumOfArray.js
@@ -1,11 +1,36 @@
+let input = document.querySelector('#numInp')
+let addBtn = document.querySelector('#addBtn')
+let sumBtn = document.querySelector('#sumBtn')
+let htmlArray = document.querySelector('#arrDisp')
+let display = document.querySelector('#currVal')
+
+
+let userArray = []
+function addToArray(val) {
+   userArray.push(val)
+   console.log(userArray)
+   htmlArray.innerText = userArray
+}
+
+addBtn.addEventListener('click', ()=>{
+   addToArray(input.value)
+   console.log(input.value)
+})
+
+sumBtn.addEventListener('click', ()=>{
+    sumOfArray(userArray)
+})
+
+
+
+
 let myArray = [2,5,7,10,50]
 
 function sumOfArray(array) {
     let sum = 0
     for (let i = 0; i < array.length; i++) {
-        sum += array[i];
+        sum = parseInt(sum) + parseInt(array[i]);
         
     }
-    console.log(sum)
+    display.innerText = sum
 }
-sumOfArray(myArray)
